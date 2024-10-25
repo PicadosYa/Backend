5f6eef8c0bb708f4e2a80fd5fe014dad87d6828f
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-10-25 19:15:16 -0300

    agregando prefijo

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -3,14 +3,16 @@ package api
 import "github.com/labstack/echo/v4"
 
 func (a *API) RegisterRoutes(e *echo.Echo) {
-	users := e.Group("/users")
+	apiGroup := e.Group("/api")
+
+	users := apiGroup.Group("/users")
 	users.POST("/register", a.RegisterUser)
 	users.POST("/login", a.LoginUser)
 
 	// ###################
 	// Fields Endpoints
 	// ###################
-	fields := e.Group("/fields")
+	fields := apiGroup.Group("/fields")
 	fields.GET("", a.GetFields)
 	fields.GET("/:id", a.GetField)
 	fields.POST("", a.CreateField)
